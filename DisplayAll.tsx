fe6ab47b54ed9176cffb2d8137645773e8933de8
Dasari-Mani-Keerthana <22bk1a0545@stpetershyd.com>
2025-02-18 14:11:55 +0530

    react

---
+++ DisplayAll.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+export default function DisplayAll() {
+  return (
+    <div>
+        
+        
+        <NavBar />
+        
+        
+        
+        DisplayAll</div>
+  )
+}
